1a6a76adbc2330f1a964df6bd7b60480bcf92aa5
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2021-08-12 19:42:52 -0400

    Twitch events listening (probably)

--- extension/index.js
+++ extension/index.js
@@ -5,6 +5,7 @@ const checkpoints = require("./checkpoints");
 const events = require("./events");
 const guests = require("./guests");
 const tiltify = require("./tiltify");
+const twitchie = require("./twitchie");
 
 module.exports = (nodecg) => {
   achievements(nodecg);
@@ -12,4 +13,5 @@ module.exports = (nodecg) => {
   events(nodecg);
   guests(nodecg);
   tiltify(nodecg);
+  twitchie(nodecg);
 };
