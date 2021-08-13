18bd9b8005f292d4bef78ff7386bde881a697220
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2021-08-13 10:59:58 -0400

    MAYBE I FORGOT SOME FILES LAST NIGHT
    CHRONO TRIGGER OPENING

---
+++ src/dashboard/opening.tsx
@@ -0,0 +1,19 @@
+import { h, render, FunctionComponent } from "preact";
+import { useReplicant } from "use-nodecg";
+
+const OpeningPanel: FunctionComponent<any> = () => {
+  const [start, setStart]: [boolean, any] = useReplicant("start", false);
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={start}
+        onChange={() => setStart(!start)}
+      />
+      Start the show?
+    </label>
+  );
+};
+
+const root = document.getElementById("container");
+render(<OpeningPanel />, root);
