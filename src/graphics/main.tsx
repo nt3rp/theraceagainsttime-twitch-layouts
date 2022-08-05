8ab2a9099cfc110b0cde8f81855bc4e111b79f30
NT3RP <me@nt3rp.io>
2022-08-05 09:33:57 -0400

    Adds rough panel setup
    Probably will iterate on this a bunch BUT at least helps get a sense of
    scale.

--- src/graphics/main.tsx
+++ src/graphics/main.tsx
@@ -1,9 +1,15 @@
 import { h, render } from "preact";
 
+// TODO: console.log panel positions to help with OBS settings.
 const MainPage = [
-  <div className="infoNav transparent"></div>,
+  <div className="infoNav transparent">
+    <div className="widescreen border">Video</div>
+    <div className="widescreen border">Video 2</div>
+    <div className="spacer"></div>
+    <div className="border">Funds raised</div>
+  </div>,
   <div className="display">
-    <div className="primaryVideo transparent"></div>
+    <div className="primaryVideo transparent standard"></div>
   </div>,
 ];
 
