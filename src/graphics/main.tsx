01d6dbf87c8a7544e4d0d44db3a344cb52a4ca2f
NT3RP <me@nt3rp.io>
2022-08-05 10:21:14 -0400

    Roughed in total amount
    Hard to do CSS style on the train; for now, just hardcode.

--- src/graphics/main.tsx
+++ src/graphics/main.tsx
@@ -1,4 +1,5 @@
 import { h, render } from "preact";
+import { Panel } from "./components/panel";
 
 // TODO: console.log panel positions to help with OBS settings.
 const MainPage = [
@@ -6,7 +7,28 @@ const MainPage = [
     <div className="widescreen border">Video</div>
     <div className="widescreen border">Video 2</div>
     <div className="spacer"></div>
-    <div className="border">Funds raised</div>
+    <Panel className="border">
+      <div className="label" style={{ paddingBottom: "8px" }}>
+        Raised for Trans Lifeline:
+      </div>
+      <div
+        className="details"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "baseline",
+        }}
+      >
+        <span
+          className="current"
+          style={{ flex: 1, textAlign: "right", fontSize: "2em" }}
+        >
+          $135.00
+        </span>
+        <span className="divider">/</span>
+        <span className="goal">2300.00</span>
+      </div>
+    </Panel>
   </div>,
   <div className="display">
     <div className="primaryVideo transparent standard"></div>
